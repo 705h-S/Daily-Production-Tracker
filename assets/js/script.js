1c4c9d3930b7f5c9fb17773b9274892688c3bbe6
705H-S <93358442+705h-S@users.noreply.github.com>
2022-01-06 16:29:41 -0800

    added psudo code to .js

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+// when webpage is opened current day is displayed using moment () I am also going to include the time
+// --I'll create two variables one for the date and the other for time 
+// -- var = date format " ddd, MMM-D-YYYY" =  Sat, Jan-01-2022
+// --var = Time format "h:mm:ss a" =  3:33:15 PM
+//         #########
+//  I'll have a section for every hour of the day that is color coded when it is the current hour, past time, furture time 
+// -- Ill create a new var "now" and it'll be equal to var=date. it'll be formated in time so "h" <- focusing on hour element 
+// -- if curent hour is equal to a section it'll be red
+// -- elseif section is less than curent hour it'll be grey
+//-- else section is greater than current hour it'll be green
+//       #########
+// inside those sections user can input a string 
+// -- section will have input tag with a type of text so user can input string 
+// user's input of string can be locally saved if user manually press save button
+// -- create an addlistener event on save button
+// -- when button it 'clicked' the user input in that section will be saved using localstorage.setitem.
+
+
+
+
